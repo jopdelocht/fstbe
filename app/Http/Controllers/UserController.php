c35db26852072d470e87930823d5b8f6ee101566
jopdel <opdelochtjohn@gmail.com>
2024-04-09 09:06:10 +0200

    update and remove user role and gamecode

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,4 +12,28 @@ class UserController extends Controller
         $users = DB::table('users')->get();
         return response()->json($users);
     }
+
+    // public function getUserById($id)
+    // {
+    //     $user = DB::table('users')->where('id', $id)->first();
+    //     return response()->json($user);
+    // }
+
+    public function updateUserRoleAndGamecode($id, Request $request)
+    {
+        DB::table('users')->where('id', $id)->update([
+            'role' => $request->input('role'),
+            'gamecode' => $request->input('gamecode')
+        ]);
+        return response()->json(['message' => 'User role and gamecode updated successfully'], 200);
+    }
+
+    public function removeUserRoleAndGamecode($id)
+    {
+        DB::table('users')->where('id', $id)->update([
+            'role' => NULL,
+            'gamecode' => NULL
+        ]);
+        return response()->json(['message' => 'User role and gamecode removed successfully'], 200);
+    }
 }
